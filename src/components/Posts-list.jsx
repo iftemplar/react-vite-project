fe00d18d662402def34c66535b8226a9810e9448
Mykola Samila <samila.mykola@gmail.com>
2023-04-21 15:38:49 +0200

    Added event listener to show data in console. Added NewPost module.

--- src/components/Posts-list.jsx
+++ src/components/Posts-list.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post.jsx";
 import classes from "./Posts-list.css"
+import NewPost from "./NewPost.jsx";
 
 function PostsList() {
     return(
-        <ul className='posts'>
-            <Post name="Mykola" text="React Course" />
-            <Post name="Rostyslav" text="Write More" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className='posts'>
+                <Post name="Mykola" text="React Course" />
+                <Post name="Rostyslav" text="Write More" />
+            </ul>
+        </>
     )
 }
 
